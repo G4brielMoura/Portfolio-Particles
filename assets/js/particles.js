92ab5ed1066082550ff8ba54167abdccc27add6d
G4brielMoura <pereirbiel@gmail.com>
2024-07-21 21:18:21 -0300

    cores-edit

--- assets/js/particles.js
+++ assets/js/particles.js
@@ -50,7 +50,7 @@ particlesJS("particles-js", {
     line_linked: {
       enable: true,
       distance: 150,
-      color: "#fff",
+      color: "#7F00FF",
       opacity: 0.4,
       width: 1,
     },
